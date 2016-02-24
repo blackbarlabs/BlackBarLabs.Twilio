8f43dd2748e2e3d9e996e698c76a73c3910959f5
Keith Holloway <keith@eastfive.com>
2016-02-24 17:37:11 -0600

    Genesis

---
+++ BlackBarLabs.Twilio.Tests/MessengerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackBarLabs.Twilio.Tests
+{
+    [TestClass]
+    public class MessengerTest
+    {
+        [Ignore]
+        [TestMethod]
+        public void SendEmail()
+        {
+            var accountSid = System.Configuration.ConfigurationManager.AppSettings["Twilio.AccountSid"];
+            var authToken = System.Configuration.ConfigurationManager.AppSettings["Twilio.AuthToken"];
+            var fromNumber = System.Configuration.ConfigurationManager.AppSettings["Twilio.TestFromNumber"];
+            var toNumber = System.Configuration.ConfigurationManager.AppSettings["Twilio.TestToNumber"];
+            var messenger = new Messenger(accountSid, authToken);
+            messenger.SendSmsMessage(fromNumber, toNumber, "Check us out at http://www.eastfive.com");
+        }
+    }
+}
---
+++ BlackBarLabs.Twilio/Messenger.cs
@@ -0,0 +1,29 @@
+﻿using Twilio;
+
+namespace BlackBarLabs.Twilio
+{
+    public class Messenger : Web.ISendSmsService
+    {
+        private readonly string accountSid;
+        private readonly string authToken;
+
+        public Messenger(string accountSid, string authToken)
+        {
+            this.accountSid = accountSid;
+            this.authToken = authToken;
+        }
+
+        public bool SendSmsMessage(string fromNumber, string toNumber, string text)
+        {
+            var twilio = new TwilioRestClient(accountSid, authToken);
+            var message = twilio.SendMessage(fromNumber, toNumber, text);
+
+            if (message.RestException != null)
+            {
+                var error = message.RestException.Message;
+                // handle the error ...
+            }
+            return true;
+        }
+    }
+}
